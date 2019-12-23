cfe8b500609d690480688231ca67515370797744
Florent GONON <florent.gonon33@gmail.com>
2019-12-24 00:58:14 +0100

    begin 2nd section skills

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,11 @@ import Typed from 'typed.js';
 
 import "../plugins/solidnav.js";
 import "../plugins/sendbtn.js";
-import "../plugins/typed.js";
 
 import { formspree } from "plugins/formspree.js";
 import { typed } from "plugins/typed.js";
+import { skills } from "plugins/skills.js";
 
 formspree();
 typed();
+skills();
