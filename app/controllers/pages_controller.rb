0c437632ed2fd106b82f13895bd43c20a9577c42
Florent GONON <florent.gonon33@gmail.com>
2019-12-26 14:52:03 +0100

    add download link for CV & add image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download_pdf
+  send_file(
+    "#{Rails.root}/public/CV.pdf",
+    filename: "CV-FlorentGONON.pdf",
+    type: "application/pdf"
+  )
+  end
 end
